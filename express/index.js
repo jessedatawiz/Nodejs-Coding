a7b63a31d77385db4e25142e836837ca88e6f376
jesse-datascience <jesser.fis@hotmail.com>
2024-02-03 18:16:32 -0300

    defined working directories

--- express/index.js
+++ express/index.js
@@ -1,11 +1,18 @@
-// Express Aplication
+// Express application
 import express from 'express';
-
+import { fileURLToPath } from 'url';
+import { dirname, sep } from 'path';
 // configuration
 const
-    cfg = {
-        port: process.env.PORT || 3000
-    };
+__dirname = dirname(fileURLToPath( import.meta.url )) + sep,
+cfg = {
+port: process.env.PORT || 3000,
+dir: {
+root: __dirname,
+static: __dirname + 'static' + sep
+}
+};
+console.dir(cfg, { depth: null, color: true });
 
 // Express initiation
 const app = express();
@@ -16,11 +23,24 @@ app.get('/', (req, res) => {
 });
 
 //another route
-app.get('/hello', (req, res) => {
+app.get('/hello/', (req, res) => {
     res.send('Hello There!');
 });
+
+// log every request to the terminal
+app.use((req, res, next) => {
+    console.log(req.url);
+    next();
+    });    
+
+// serve static assets
+app.use(express.static( cfg.dir.static ));
+
 
 // start server
 app.listen(cfg.port, () => {
     console.log(`Example app at ${ cfg.port }`);
 });
+
+// exports defaults
+export { cfg, app };
